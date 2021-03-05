2bc812d7cb73353fac1d1c2efc8e6d5a7ca20253
amaidah <a.maidah@gmail.com>
2021-03-04 22:10:01 -0800

    add typescript with linting and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
+  ignorePatterns: ["node_modules/*", ".next/*", ".out/*", ".eslintrc.js", "!.prettierrc.js"], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  extends: ["eslint:recommended"],
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx", "**/*.js"],
+      parser: "@typescript-eslint/parser",
+      settings: { react: { version: "detect" } },
+      env: {
+        browser: true,
+        node: true,
+        es6: true,
+      },
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended", 
+        "plugin:react/recommended", 
+        "plugin:react-hooks/recommended", 
+        "plugin:jsx-a11y/recommended", 
+        'plugin:prettier/recommended', // Prettier always needs to be last
+      ],
+      rules: {
+        // We will use TypeScript's types for component props instead
+        "react/prop-types": "off",
+
+        // No need to import React when using Next.js
+        "react/react-in-jsx-scope": "off",
+
+        // This rule is not compatible with Next.js's <Link /> components
+        "jsx-a11y/anchor-is-valid": "off",
+
+        // Why would you want unused vars?
+        "@typescript-eslint/no-unused-vars": ["error"],
+
+        // I suggest this setting for requiring return types on functions only where useful
+        "@typescript-eslint/explicit-function-return-type": [
+          "warn",
+          {
+            allowExpressions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+          },
+        ],
+        
+        // Prettier always needs to be last
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }], 
+      },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+};
